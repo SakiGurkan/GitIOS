13fea5591b79affd3c704fef3850b471148ebfbe
Gürkan Saki <>
2021-03-10 17:44:21 +0300

    second commit

---
+++ GitTextProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTextProject
+//
+//  Created by Gürkan Saki on 10.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("git")
+    }
+
+
+}
+
